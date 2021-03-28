cae6b44922c5e188208e9538ba49a2e96ebdfbd8
mehrbarz <mehrbarzegar@gmail.com>
2021-03-28 13:37:43 +0200

    this.

---
+++ class_constructors.dart
@@ -0,0 +1,24 @@
+void main() {
+  Human jenny = Human(height: 43, weight: 45);
+  print(jenny.height);
+}
+
+class Human {
+  double height ;
+  double weight ;
+
+  Human({this.height, this.weight}) ;
+}
+  
+  /**
+   * or 
+   * Human ({double height . double weight}){
+
+   this.height = height;
+   this.weight = weight; 
+      }
+   * 
+   * 
+   * */
+  
+
